2a72b4b50fe2c8d3fc1740c1111da4eaf3bec23c
Ramon Bisswanger <bisswang@adobe.com>
2022-06-23 13:02:43 +0200

    Added IMS /userinfo function

---
+++ ims/get_userinfo_test.go
@@ -0,0 +1,77 @@
+// Copyright 2021 Adobe. All rights reserved.
+// This file is licensed to you under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may obtain a copy
+// of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under
+// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+// OF ANY KIND, either express or implied. See the License for the specific language
+// governing permissions and limitations under the License.
+
+package ims_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/adobe/ims-go/ims"
+)
+
+func TestGetUserInfo(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			t.Fatalf("invalid method: %v", r.Method)
+		}
+		if v := r.Header.Get("authorization"); v != "Bearer accessToken" {
+			t.Fatalf("invalid authorization header: %v", v)
+		}
+
+		fmt.Fprint(w, `{"foo":"bar"}`)
+	}))
+	defer s.Close()
+
+	c, err := ims.NewClient(&ims.ClientConfig{
+		URL: s.URL,
+	})
+	if err != nil {
+		t.Fatalf("create client: %v", err)
+	}
+
+	res, err := c.GetUserInfo(&ims.GetUserInfoRequest{
+		AccessToken: "accessToken",
+	})
+	if err != nil {
+		t.Fatalf("get user info: %v", err)
+	}
+
+	if body := string(res.Body); body != `{"foo":"bar"}` {
+		t.Fatalf("invalid body: %v", body)
+	}
+}
+
+func TestGetUserInfoEmptyErrorResponse(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer s.Close()
+
+	c, err := ims.NewClient(&ims.ClientConfig{
+		URL: s.URL,
+	})
+	if err != nil {
+		t.Fatalf("create client: %v", err)
+	}
+
+	res, err := c.GetUserInfo(&ims.GetUserInfoRequest{})
+	if res != nil {
+		t.Fatalf("non-nil response returned")
+	}
+	if err == nil {
+		t.Fatalf("nil error returned")
+	}
+	if _, ok := ims.IsError(err); !ok {
+		t.Fatalf("invalid error type: %v", err)
+	}
+}
